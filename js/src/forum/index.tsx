6b28480dcb33ad6b1f75589410430cbef6ee0f17
Farzoqe Khan <farzoqe@gmail.com>
2025-04-08 18:42:43 +0500

    updates

--- js/src/forum/index.tsx
+++ js/src/forum/index.tsx
@@ -1,6 +1,31 @@
 import { extend } from 'flarum/common/extend';
 import HeaderPrimary from 'flarum/forum/components/HeaderPrimary';
+import LoginModal from 'flarum/forum/components/LoginModal';
 
 extend(HeaderPrimary.prototype, 'items', function(items) {
   items.add('google', <a href="https://google.com">Google</a>);
+});
+
+
+extend(LoginModal.prototype, 'fields', function (items) {
+  const customLink = 'https://your-custom-reset-link.com';
+
+  items.add(
+    'identification',
+    <div className="Form-group">
+      <input
+        className="FormControl"
+        name="identification"
+        type="text"
+        placeholder={'test'}
+        aria-label={'test'}
+        // bidi={this.identification}
+        // disabled={this.loading}
+      />
+    </div>,
+    30
+  );
+});
+extend(LoginModal.prototype, 'footer', function (items) {
+  return 'test'
 });
