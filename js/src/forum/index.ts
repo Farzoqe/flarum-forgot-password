8e4239a8f2f49f0730c5b6c26e4ececef3ec37f8
Farzoqe Khan <farzoqe@gmail.com>
2025-04-08 16:54:44 +0500

    build

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,17 +1,7 @@
 import app from 'flarum/forum/app';
-import { extend } from 'flarum/common/extend';
-import LoginModal from 'flarum/forum/components/LoginModal';
 
-extend(LoginModal.prototype, 'footer', function (items:any) {
-  const customLink = 'https://findtheseven.com/forgot-password';
+export { default as extend } from './extend';
 
-  items.replace('forgotPassword', m(
-    'a',
-    {
-      href: customLink,
-      target: '_blank',
-      rel: 'noopener noreferrer',
-    },
-    app.translator.trans('core.forum.login.forgot_password_link')
-  ));
+app.initializers.add('farzoqe-fts-forgot-password', () => {
+  console.log('[farzoqe/flarum-fts-forgot-password] Hello, forum!');
 });
