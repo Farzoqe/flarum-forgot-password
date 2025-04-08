ea3bdaf931c5ff420d45b3ab0d6de5a008bfc0cc
Farzoqe Khan <farzoqe@gmail.com>
2025-04-08 17:31:08 +0500

    build

--- js/src/common/index.ts
+++ js/src/common/index.ts
@@ -1,21 +1,5 @@
 import app from 'flarum/common/app';
-import {extend} from "flarum/common/extend";
-import LoginModal from "flarum/forum/components/LogInModal";
 
 app.initializers.add('farzoqe-fts-forgot-password-common', () => {
   console.log('[farzoqe/flarum-fts-forgot-password] Hello, forum and admin!');
-});
-
-
-extend(LoginModal.prototype, 'footer', function (items: any) {
-  const customLink = 'https://findtheseven.com/forgot-password';
-  items.replace('forgotPassword', m(
-    'a',
-    {
-      href: customLink,
-      target: '_blank',
-      rel: 'noopener noreferrer',
-    },
-    app.translator.trans('core.forum.login.forgot_password_link')
-  ));
 });
--- js/src/forum/extend.ts
+++ js/src/forum/extend.ts
@@ -1,12 +1,8 @@
 import Extend from 'flarum/common/extenders';
 import commonExtend from '../common/extend';
 
-import { extend } from 'flarum/common/extend';
-import LoginModal from 'flarum/forum/components/LoginModal';
-
-
 export default [
   ...commonExtend,
 
-
+  // Add your forum extenders here
 ];
